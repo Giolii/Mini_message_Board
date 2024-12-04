ce429438f8c62483b99693c8f2aeb77d3296c694
Gi <160989051+Giolii@users.noreply.github.com>
2024-12-03 16:15:40 -0800

    First

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const port = 3000;
+
+const indexRouter = require("./routes/indexRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+app.listen(port, () => {
+  console.log("Listening on port ", port);
+});
