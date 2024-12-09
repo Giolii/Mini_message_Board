502bc5c9c8c29e6ce028f4f43c5ffe8b659e1c36
Gi <160989051+Giolii@users.noreply.github.com>
2024-12-09 11:10:42 -0800

    FIxed populate

--- db/queries.js
+++ db/queries.js
@@ -53,4 +53,5 @@ module.exports = {
   getAllMessages,
   insertMessage,
   deleteAll,
+  populatedb,
 };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,7 +3,7 @@ const indexRouter = Router();
 const controller = require("../controllers/messagesController");
 
 indexRouter.get("/", controller.getMessages);
-indexRouter.get("/populatedb", controller.populatedb);
+indexRouter.post("/populatedb", controller.populatedb);
 indexRouter.get("/details/:id", controller.getDetails);
 indexRouter.post("/new", controller.CreateMessagePost);
 indexRouter.post("/deleteAll", controller.deleteAll);
